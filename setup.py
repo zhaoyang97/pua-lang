1714727f1cfd86f629f7842a38014e208827e863
zhaoyang97 <1173201671@qq.com>
2022-01-02 19:55:29 +0800

    fix a bug

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     summary='PUA Programming Language written in Python.',
     version='0.1.4',
     author='zy',
-    author_email='consolelogi@outlook.com',
+    author_email='',
     install_requires = ["setuptools"],
     include_package_data = True,
     packages=find_packages(exclude=['test']),
@@ -21,6 +21,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
